426c790b794484669e24d6a8861e16034fe8c0fd
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-27 10:57:55 -0500

    Added the assets loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
