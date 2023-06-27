ff536d317a19833d962a38998206a00aae67a60d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-27 16:25:50 -0500

    Added a module to count and retrieve the likes

---
+++ src/modules/likes.js
@@ -0,0 +1,33 @@
+const countLikes = async (event) => {
+  try {
+    const likes = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/FjhFMUdws0lCxR3eXCdS/likes', {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: `${event.target.parentNode.parentNode.id}`,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    return likes;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+const retrieveLikes = async (id) => {
+  try {
+    const numberLikes = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/FjhFMUdws0lCxR3eXCdS/likes', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const totalLikes = numberLikes.find((obj) => obj.item_id === id);
+    return totalLikes.likes;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export { countLikes, retrieveLikes };
