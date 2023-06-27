1f07398c6cd5d39482e6e37117d9e5cca866dc3c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-27 14:50:38 -0500

    Added a div container to the like icon and title, and classes to the elementes

--- src/modules/fetch-movies.js
+++ src/modules/fetch-movies.js
@@ -23,32 +23,27 @@ const render = async () => {
     const poster = show.image ? show.image.medium : '';
     const title = show.name;
     const { id } = show;
-    const actors = 'N/A';
-    const rating = show.rating.average;
     const showDiv = document.createElement('div');
     const posterImg = document.createElement('img');
-    const titleH2 = document.createElement('h2');
-    const actorsP = document.createElement('p');
-    const summaryP = document.createElement('p');
-    const ratingP = document.createElement('p');
     const icons = document.createElement('span');
+    const titleContainer = document.createElement('div');
     posterImg.src = poster;
-    titleH2.innerText = title;
-    actorsP.innerText = `Actors: ${actors}`;
     showDiv.id = `${id}`;
-
-    ratingP.innerText = `Rating: ${rating}`;
-    icons.innerHTML = `<button><i class="fa-regular fa-heart"></i> Like</button> 
+    showDiv.classList.add('series');
+    titleContainer.classList.add('div-title');
+    titleContainer.innerHTML = `
+      <h2>${title}</h2>
+      <i class="fa-regular fa-heart"></i>
+    `;
+    icons.classList.add('span-icons');
+    icons.innerHTML = `
       &nbsp; 
       <button><i class="fa-regular fa-comment"></i> Comment</button>
       &nbsp; 
       <button><i class="fa-regular fa-clock"></i> Reserve</button>`;
 
     showDiv.appendChild(posterImg);
-    showDiv.appendChild(titleH2);
-    showDiv.appendChild(actorsP);
-    showDiv.appendChild(summaryP);
-    showDiv.appendChild(ratingP);
+    showDiv.appendChild(titleContainer);
     showDiv.appendChild(icons);
     tvShowsDiv.appendChild(showDiv);
   });
