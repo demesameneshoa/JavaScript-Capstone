6478184cbf3d48bc66c5c5b1aea9fc90bccb3c26
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-27 16:36:09 -0500

    Include an await to render and listeners to the heart icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
 import render from './modules/fetch-movies.js';
 import logo from './assets/logo-white.png';
+import { countLikes } from './modules/likes.js';
 
 document.getElementById('logo-img').setAttribute('src', logo);
-render();
+await render();
+
+const heartIcons = document.querySelectorAll('.fa-heart');
+
+heartIcons.forEach((icon) => icon.addEventListener('click', countLikes));
