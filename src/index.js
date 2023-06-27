776c0aeddba6ec339eb1eef2005ae0e1917365dc
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-27 11:00:11 -0500

    Render the movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import render from './modules/fetch-movies.js';
+import logo from './assets/logo-white.png';
+
+document.getElementById('logo-img').setAttribute('src', logo);
+render();
