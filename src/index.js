a4a68a7fb50f22747912c9e3d52723db44fef0c3
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-28 11:30:30 -0500

    Include the number of items in the span element

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import render from './modules/fetch-movies.js';
 import logo from './assets/logo-white.png';
 import renderpopup from './modules/comment.js';
 import { countLikes } from './modules/likes.js';
+import itemsCounter from './modules/items-counter.js';
 
 document.getElementById('logo-img').setAttribute('src', logo);
 await render();
 
 const heartIcons = document.querySelectorAll('.fa-heart');
+const spanItems = document.getElementById('items-count');
+// Add the number of items in the span.
+spanItems.textContent = ` (${itemsCounter()})`;
 
 heartIcons.forEach((icon) => icon.addEventListener('click', (event) => {
   countLikes(event);
