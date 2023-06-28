fbdc05bddaee45bb45af1e556e0adb653b0ca19f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-27 22:37:53 -0500

    Added a parentNode to select the right div container

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -3,7 +3,7 @@ const countLikes = async (event) => {
     return await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/FjhFMUdws0lCxR3eXCdS/likes', {
       method: 'POST',
       body: JSON.stringify({
-        item_id: `${event.target.parentNode.parentNode.id}`,
+        item_id: `${event.target.parentNode.parentNode.parentNode.id}`,
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
